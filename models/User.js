568971d105f52586d51ed4f82eccf1ab79c09a83
Stefano Cavalli <stefanocavalli@outlook.com>
2025-04-17 10:21:31 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);

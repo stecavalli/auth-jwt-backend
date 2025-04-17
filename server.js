ea318170689f2d6a531f6611ddd3d75f23e3a3d0
Stefano Cavalli <stefanocavalli@outlook.com>
2025-04-17 19:54:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: "https://TUO_NOME_SITO.netlify.app", // Cambia con il tuo sito reale
+  origin: "https://react-jwt-pswcriptate.netlify.app", // Cambia con il tuo sito reale
   credentials: true
 }));
 app.use(cookieParser());

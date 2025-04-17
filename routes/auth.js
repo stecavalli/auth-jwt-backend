73c71b361c6896389a446ca4c4225e74676b49e2
Stefano Cavalli <stefanocavalli@outlook.com>
2025-04-17 12:20:16 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -56,7 +56,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "None",  // Assicurati che il frontend faccia richieste cross-origin
+      sameSite: "None",  // Richieste cross-origin
       secure: true,      // Usa HTTPS
       maxAge: 24 * 60 * 60 * 1000, // 1 giorno
     });
